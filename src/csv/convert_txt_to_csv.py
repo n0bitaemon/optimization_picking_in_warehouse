39f5e9ec1c14bf15220e6d7d1bac690065e55158
Tuan Minh Tran <92513539+7und3dpy@users.noreply.github.com>
2023-02-04 10:41:45 +0700

    Update convert_txt_to_csv.py

--- src/csv/convert_txt_to_csv.py
+++ src/csv/convert_txt_to_csv.py
@@ -1,5 +1,5 @@
 import pandas as pd
-folder_file_txt = input() 
-read_file = pd.read_csv(r'{}'.format(folder_file_txt))
+folder_file_txt_destination = input() 
+read_file = pd.read_csv(r'{}'.format(folder_file_txt_destination))
 file_destination_csv = input()
 read_file.to_csv(r'{}'.format(file_destination_csv),index= None)
